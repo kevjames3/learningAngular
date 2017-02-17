4233a50f2f3ae6f07dfecc4c3a3e43b585a96562
Kevin McIntosh <kevjames3@gmail.com>
2017-02-17 07:54:21 -0800

    Initial commit of the tutorial

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { HeroDetailComponent } from './hero-detail.component';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
